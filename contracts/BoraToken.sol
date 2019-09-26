39bc344e9bf6455746fb8752f0d84d74e3f477b4
boraecosystem-dev <36653198+boraecosystem-dev@users.noreply.github.com>
2019-09-26 17:46:10 +0900

    - addOwnership() -> addAdmin() - renounceOwnership() -> renounceAdmin() - inherit Ownable - add newOwner to admin when transfers ownership

--- contracts/BoraToken.sol
+++ contracts/BoraToken.sol
@@ -5,13 +5,16 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./BlacklistedRole.sol";
 import "./LockedToken.sol";
 
-contract BoraToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Burnable, ERC20Pausable, BlacklistedRole {
+contract BoraToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Burnable, ERC20Pausable, Ownable, BlacklistedRole {
 
     event Lock(address token, address beneficiary, uint256 amount, uint256 releaseTime);
     event Burn(address to, uint256 amount, uint256 totalSupply);
+    event AdminAdded(address indexed account);
+    event AdminRemoved(address indexed account);
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor(uint256 initialSupply) ERC20Detailed("BORA", "BORA", 18) ERC20Capped(initialSupply) public {
@@ -73,36 +76,46 @@ contract BoraToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Burnable, ERC20Pau
     }
 
     /**
-     * @dev Adds ownership (MinterRole, PauserRole, WhitelistAdminRole) to `newOwner`.
+     * @dev Adds admin (MinterRole, PauserRole, WhitelistAdminRole) to `newAdmin`.
      *
-     * Emits a `OwnershipTransferred` event.
+     * Emits a `AdminAdded` event.
      *
      * Requirements:
      *
      * - the caller must have the `MinterRole`, `PauserRole` and `WhitelistAdminRole`.
-     * - `newOwner` cannot be the zero address.
+     * - `newAdmin` cannot be the zero address.
      */
-    function addOwnership(address newOwner) public onlyMinter onlyPauser onlyWhitelistAdmin {
-        require(newOwner != address(0), "BoraToken: add ownership to the zero address");
-        super.addMinter(newOwner);
-        super.addPauser(newOwner);
-        super.addWhitelistAdmin(newOwner);
-        emit OwnershipTransferred(msg.sender, newOwner);
+    function addAdmin(address newAdmin) public onlyMinter onlyPauser onlyWhitelistAdmin {
+        require(newAdmin != address(0), "BoraToken: add admin of the zero address");
+        super.addMinter(newAdmin);
+        super.addPauser(newAdmin);
+        super.addWhitelistAdmin(newAdmin);
+        emit AdminAdded(newAdmin);
     }
 
     /**
-     * @dev Renounces ownership (MinterRole, PauserRole, WhitelistAdminRole) from the caller.
+     * @dev Renounces admin (MinterRole, PauserRole, WhitelistAdminRole) from the caller.
      *
-     * Emits a `OwnershipTransferred` event with `newOwner` set to the zero address.
+     * Emits a `AdminRemoved` event.
      *
      * Requirements:
      *
      * - the caller must have the `MinterRole`, `PauserRole` and `WhitelistAdminRole`.
      */
-    function renounceOwnership() public onlyMinter onlyPauser onlyWhitelistAdmin {
+    function renounceAdmin() public onlyMinter onlyPauser onlyWhitelistAdmin {
         super.renounceMinter();
         super.renouncePauser();
         super.renounceWhitelistAdmin();
-        emit OwnershipTransferred(msg.sender, address(0));
+        emit AdminRemoved(msg.sender);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     * Also addds admin (MinterRole, PauserRole, WhitelistAdminRole) to `newAdmin`.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        addAdmin(newOwner);
+        Ownable.transferOwnership(newOwner);
     }
 }
